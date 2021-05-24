78b42407a7d32ee0e68b1f7d432b8d1dbcae523e
vihangamalshan1998 <68722779+vihangamalshan1998@users.noreply.github.com>
2021-05-24 11:13:52 +0530

    added chnages

--- routes/AddResearchroute.js
+++ routes/AddResearchroute.js
@@ -2,14 +2,6 @@ const router = require("express").Router();
 let Reseatchreviws = require("../models/AddResearches");
 const nodemailer = require('nodemailer');
 
-let mailTransporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'applicationframeworkproject@gmail.com',
-        pass: 'malisha1996'
-    }
-});
-
 router.route("/add").post((req,res)=>{
 
     const research_name = req.body.research_name;
@@ -22,24 +14,8 @@ router.route("/add").post((req,res)=>{
     })
 
     newReviwer.save().then(()=>{
-        let mailDetails = {
-            from: 'applicationframeworkproject@gmail.com',
-            to: newReviwer.email,
-            subject: 'YOU ADDED AS A REVIWER IN SLIIT-ICMS',
-            text: 'Mr./Mrs. ' + newReviwer.first_name + ' ' + newReviwer.last_name + ",\n\n" 
-                    + "Congradulations!\n\n" 
-                    + "Succesfully, You added as a reviwer in SLIIT_ICMS\n\n"
-                    + "You password is " + newReviwer.password + " \n\n"
-                    + "make sure after you loging update your password and profile details.\n\n"       
-        };
-        mailTransporter.sendMail(mailDetails, function(err, data) {
-            if(err) {
-                console.log('Error Occurs');
-            } else {
-                console.log('Email sent successfully');
-            }
-        });
-        res.json("Reviwer Added")
+
+        res.json("Research Added")
     }).catch((err)=>{
         console.log(err);
     })
